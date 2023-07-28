bcbb575548224e1cd23eb04c107d1f15acb5b786
Lisa Wu Zheng <lisawuzheng@lisas-imac.mynetworksettings.com>
2023-07-27 21:09:00 -0400

    added button to authenticate

--- src/Components/Authenticate.jsx
+++ src/Components/Authenticate.jsx
@@ -1,8 +1,14 @@
 const Authenticate = ({ token }) => {
   console.log(token)
+
+  const handleClick = async () => {
+
+  };
+
   return (
     <>
       <h2>Authenticate</h2>
+      <button onClick={handleClick}>Authenticate Token</button>
     </>
   );
 };
