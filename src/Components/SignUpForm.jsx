b4e3ed7f665ac49d41d64c40b9e9da7903b8f869
Lisa Wu Zheng <lisawuzheng@lisas-imac.mynetworksettings.com>
2023-07-27 20:19:36 -0400

    added form input and submit functionality

--- src/Components/SignUpForm.jsx
+++ src/Components/SignUpForm.jsx
@@ -5,18 +5,31 @@ const SignUpForm = () => {
   const [password, setPassword] = useState(``);
   const [error, setError] = useState(null);
 
+  const updateUsernameInput = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const updatePasswordInput = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(`hello`);
+  };
+
   return (
     <>
       <h2>Sign Up</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Username:
-          <input />
+          <input value={username} onChange={updateUsernameInput} />
         </label>
         <br />
         <label>
           Password:
-          <input />
+          <input value={password} onChange={updatePasswordInput} />
         </label>
         <br />
         <button>Submit</button>
