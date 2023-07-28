9008f566148bdb1d4418bff4ae52d483ca00bfeb
Lisa Wu Zheng <lisawuzheng@lisas-imac.mynetworksettings.com>
2023-07-27 20:36:44 -0400

    form works yay

--- src/Components/SignUpForm.jsx
+++ src/Components/SignUpForm.jsx
@@ -16,20 +16,42 @@ const SignUpForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     console.log(`hello`);
+
+    try {
+      const response = await fetch(
+        `https://fsa-jwt-practice.herokuapp.com/signup`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        }
+      );
+      const data = await response.json();
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <>
       <h2>Sign Up</h2>
+      {error ? <p>{error}</p> : null}
       <form onSubmit={handleSubmit}>
         <label>
           Username:
-          <input value={username} onChange={updateUsernameInput} />
+          <input type='text' value={username} onChange={updateUsernameInput} required />
         </label>
         <br />
         <label>
           Password:
-          <input value={password} onChange={updatePasswordInput} />
+          <input type='password' value={password} onChange={updatePasswordInput} required />
         </label>
         <br />
         <button>Submit</button>
